954ed20ec6b02c1199b79552f91f6e0003f994ba
rflban <nabiev.alsn@yandex.ru>
2019-10-22 20:59:02 +0300

    add research test tables, fix matrix algoes

--- lab_01/include/template/DamerauLevenshtein.hxx
+++ lab_01/include/template/DamerauLevenshtein.hxx
@@ -23,18 +23,19 @@ int DamerauLevenshtein<_Word_t>::distance(_Word_t w1, int n1, _Word_t w2, int n2
         D[i][0] = D[i - 1][0] + deleteCost;
 
         for (int j = 1; j <= n2; j++) {
-            if (w1[i - 1] == w2[j - 1]) {
-                D[i][j] = D[i - 1][j - 1];
-            }
-            else {
-                D[i][j] = std::min(std::min(D[i - 1][j] + deleteCost,
-                                            D[i][j - 1] + insertCost),
-                                   D[i - 1][j - 1] + replaceCost);
+            int replaceCost = this->replaceCost;
 
-                if (i > 1 && j > 1) {
-                    if (w1[i - 1] == w2[j - 2] && w1[i - 2] == w2[j - 1]) {
-                        D[i][j] = std::min(D[i - 2][j - 2] + transposeCost, D[i][j]);
-                    }
+            if (w1[i - 1] == w2[j - 1]) {
+                replaceCost = 0;
+            }
+
+            D[i][j] = std::min(std::min(D[i - 1][j] + deleteCost,
+                                        D[i][j - 1] + insertCost),
+                               D[i - 1][j - 1] + replaceCost);
+
+            if (i > 1 && j > 1) {
+                if (w1[i - 1] == w2[j - 2] && w1[i - 2] == w2[j - 1]) {
+                    D[i][j] = std::min(D[i - 2][j - 2] + transposeCost, D[i][j]);
                 }
             }
         }
@@ -59,18 +60,19 @@ int **DamerauLevenshtein<_Word_t>::raw(_Word_t w1, int n1, _Word_t w2, int n2) {
         D[i][0] = D[i - 1][0] + deleteCost;
 
         for (int j = 1; j <= n2; j++) {
-            if (w1[i - 1] == w2[j - 1]) {
-                D[i][j] = D[i - 1][j - 1];
-            }
-            else {
-                D[i][j] = std::min(std::min(D[i - 1][j] + deleteCost,
-                                            D[i][j - 1] + insertCost),
-                                   D[i - 1][j - 1] + replaceCost);
+            int replaceCost = this->replaceCost;
 
-                if (i > 1 && j > 1) {
-                    if (w1[i - 1] == w2[j - 2] && w1[i - 2] == w2[j - 1]) {
-                        D[i][j] = std::min(D[i - 2][j - 2] + transposeCost, D[i][j]);
-                    }
+            if (w1[i - 1] == w2[j - 1]) {
+                replaceCost = 0;
+            }
+
+            D[i][j] = std::min(std::min(D[i - 1][j] + deleteCost,
+                                        D[i][j - 1] + insertCost),
+                               D[i - 1][j - 1] + replaceCost);
+
+            if (i > 1 && j > 1) {
+                if (w1[i - 1] == w2[j - 2] && w1[i - 2] == w2[j - 1]) {
+                    D[i][j] = std::min(D[i - 2][j - 2] + transposeCost, D[i][j]);
                 }
             }
         }
--- lab_01/include/template/WagnerFischer.hxx
+++ lab_01/include/template/WagnerFischer.hxx
@@ -18,14 +18,15 @@ int WagnerFischer<_Word_t>::distance(_Word_t w1, int n1, _Word_t w2, int n2) {
         D[i][0] = D[i - 1][0] + deleteCost;
 
         for (int j = 1; j <= n2; j++) {
+            int replaceCost = this->replaceCost;
+
             if (w1[i - 1] == w2[j - 1]) {
-                D[i][j] = D[i - 1][j - 1];
+                replaceCost = 0;
             }
-            else {
-                D[i][j] = std::min(std::min(D[i - 1][j] + deleteCost,
-                                            D[i][j - 1] + insertCost),
-                                   D[i - 1][j - 1] + replaceCost);
-            }
+
+            D[i][j] = std::min(std::min(D[i - 1][j] + deleteCost,
+                                        D[i][j - 1] + insertCost),
+                               D[i - 1][j - 1] + replaceCost);
         }
     }
 
@@ -48,14 +49,15 @@ int **WagnerFischer<_Word_t>::raw(_Word_t w1, int n1, _Word_t w2, int n2) {
         D[i][0] = D[i - 1][0] + deleteCost;
 
         for (int j = 1; j <= n2; j++) {
+            int replaceCost = this->replaceCost;
+
             if (w1[i - 1] == w2[j - 1]) {
-                D[i][j] = D[i - 1][j - 1];
+                replaceCost = 0;
             }
-            else {
-                D[i][j] = std::min(std::min(D[i - 1][j] + deleteCost,
-                                            D[i][j - 1] + insertCost),
-                                   D[i - 1][j - 1] + replaceCost);
-            }
+
+            D[i][j] = std::min(std::min(D[i - 1][j] + deleteCost,
+                                        D[i][j - 1] + insertCost),
+                               D[i - 1][j - 1] + replaceCost);
         }
     }
 

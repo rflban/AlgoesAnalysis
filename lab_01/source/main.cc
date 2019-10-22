3ff44163e79194f5fa734c2347a82e6934dae0d9
rflban <nabiev.alsn@yandex.ru>
2019-10-22 13:47:32 +0300

    fix input, complete impl chapter, add test data

--- lab_01/source/main.cc
+++ lab_01/source/main.cc
@@ -7,10 +7,13 @@ using namespace std;
 
 int main(int argc, char **argv) {
     bool showTime = false;
+    bool isLoop = false;
 
     std::string s1;
     std::string s2;
+
     auto tester = new EditDistanceTester<string>;
+    tester->enableOptInfo();
 
     for (int idx = 1; idx < argc; idx++) {
         switch (argv[idx][0]) {
@@ -37,19 +40,27 @@ int main(int argc, char **argv) {
             break;
         case 'T':
             showTime = false;
+            break;
+        case 'l':
+            isLoop = true;
+            break;
+        case 'L':
+            isLoop = false;
+            break;
         }
     }
 
-    while (cin.good()) {
-        getline(cin, s1);
-        getline(cin, s2);
-
+    while (getline(cin, s1), getline(cin, s2), cin.good()) {
         cout << tester->calculate(s1, s2) << " ";
 
         if (showTime)
             cout << tester->timeTestResult();
 
         cout << '\n';
+
+        if (!isLoop) {
+            break;
+        }
     }
 
     delete tester;
